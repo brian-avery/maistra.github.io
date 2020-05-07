2a9a896d16ce4770768451b4c830fec1f2020f46
Brian Avery <bavery@redhat.com>
2020-05-06 21:34:36 -0400

    Updates

--- tools/check-git-status.sh
+++ tools/check-git-status.sh
@@ -2,6 +2,8 @@
 
 if [ -z "$(git status --porcelain)" ]; then
     echo "No git changes."
+    return 0
 else
     echo "Git changes detected. Please review log and commit changes."
+    return 1
 fi
